88e45af9e67bd7143403cd09653500ee925e5073
Rebeka L Paes <133894733+rebekalacerda@users.noreply.github.com>
2026-06-26 11:36:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+    // Menu hamburguer em telas peqeunas
+    const hamburger = document.querySelector('.hamburger');
+    const navMenu = document.querySelector('.nav-menu');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        navMenu.classList.toggle('active');
+    });
+
+    // Fecha e abre o menu
+    document.querySelectorAll('.nav-menu a').forEach(link => {
+        link.addEventListener('click', () => {
+            hamburger.classList.remove('active');
+            navMenu.classList.remove('active');
+        });
+    });
